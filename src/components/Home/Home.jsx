2fbbfb853a58774923db8b45886751b9a01b92a0
SoltaneKarim <karimsoltane92@gmail.com>
2024-07-17 22:30:48 +0100

    navbar fixed

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -6,6 +6,21 @@ const Home = () => {
     <div className="container">
       
       <div className="bloc1" style={{ backgroundImage: "url(/src/assets/pic4home/pic1.png)" }}>
+      <div class="row">
+                    <div class="col-lg-6">
+                        <h2>
+                            Zortrax 3D Printer Comes with the Best Price Ever                        </h2>
+                        <a href="https://store.zortrax.com/3d-printers/zortrax-m200-plus">
+                            Order M200 Plus                        </a>
+                    </div>
+                    <div class="col-lg-6"></div>
+                </div>
+      
+
+
+
+
+
       </div>
 
       <div className="bloc2" style={{ backgroundImage: "url(/src/assets/pic4home/pic2.jpg)" }}>
--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -13,18 +13,18 @@ const Navbar = () => {
               width="100"
               height="26"
             />
-            <div className="vertical-bar"></div> {/* Added vertical bar */}
-            <span className="ml-2">Global 3D Printers Manufacturer</span>
+            <div className="vertical-bar"></div>
+            <p className="text-body-secondary mb-0 ms-2" style={{ fontSize: '15px' }}>Global 3D Printers <br /> Manufacturer</p>
           </a>
-          <div className="d-flex">
-          <button type="button" class="btn" data-bs-toggle="button">Toggle button</button>
-            <button type="button" class="btn btn-primary active" data-bs-toggle="button" aria-pressed="true">Online Store</button>
+          <div className="d-flex align-items-center">
+            <button type="button" className="btn" data-bs-toggle="button">Find Reseller</button>
+            <button type="button" className="btn btn-primary active" data-bs-toggle="button" aria-pressed="true">Online Store</button>
           </div>
         </div>
       </nav>
-      <hr /> {/* Increased width to 30% */}
+      <hr className="hr1" />
       <div>
-        <ul className="nav justify-content-center fw-medium">
+        <ul className="nav justify-content-center fw-medium ">
           <li className="nav-item">
             <a className="nav-link link-dark" aria-current="page" href="#">
               Products
@@ -51,7 +51,6 @@ const Navbar = () => {
             </a>
           </li>
         </ul>
-        
       </div>
     </div>
   );
